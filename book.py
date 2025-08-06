ab5e98d5f093a001f5d3e11f7345b7f32deac198
ApolloDev-linx <bossangel1127@gmail.com>
2025-08-06 16:16:25 -0500

    leet:Search Insert Postion JS

--- book.py
+++ book.py
@@ -1,4 +1,21 @@
 # 1.1 is unique
-def unique(s1):
+def stringCOM(s1):
+    if s1.len < 2 or s1 == "":
+        return s1
+    compress = ""
+    i = 1
+    count = 1
+    while i < s1.len:
+        i+= 1
+        if s1[i] == s1[i - 1]:
+            count+= 1
+        else:
+            compress+= s1[i - 1] + count
+            count = 1
+    compress+= s1[s1.len -1] + count
+    return compress
+
+print(stringCOM("aaaabbbbbddddlllll"))
+
 
    
